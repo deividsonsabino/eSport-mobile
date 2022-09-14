da288638d9cc22b20280f9edd2693a05ff99fecb
deividson sabino <deividsonsabino@outlook.com.br>
2022-09-14 15:40:31 -0300

    feat: add background

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { View } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View>
+    <Background>
 
-    </View>
+    </Background>
   );
 }
 
